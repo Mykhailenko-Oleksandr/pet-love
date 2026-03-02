6dca4ab902a7d95c92580612a49fc888b7178bde
Sapper <saniamykhailenko@gmail.com>
2026-03-02 17:29:26 +0200

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,16 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Manrope } from "next/font/google";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
-const roboto = Roboto({
+const manrope = Manrope({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-roboto",
+  weight: ["500", "600", "700", "800"],
+  variable: "--font-manrope",
   display: "swap",
 });
 
@@ -34,7 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable}`}>
+      <body className={`${manrope.variable}`}>
         <TanStackProvider>
           <Header />
 
