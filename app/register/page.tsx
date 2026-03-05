a4b07b5346c7a30f9120acfe04c7d58acc017b32
Sapper <saniamykhailenko@gmail.com>
2026-03-05 17:04:06 +0200

    add Title component

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,19 @@
+import Title from "@/components/Title/Title";
 import css from "./Register.module.css";
 import PetBlock from "@/components/PetBlock/PetBlock";
+import clsx from "clsx";
 
 export default function Register() {
   return (
     <section className={css.section}>
-      <div className="container">
+      <div className={clsx("container", css.registerContainer)}>
         <PetBlock page="register" />
+        <div className={css.formBox}>
+          <Title
+            title="Registration"
+            description="Thank you for your interest in our platform."
+          />
+        </div>
       </div>
     </section>
   );
---
+++ components/Title/Title.tsx
@@ -0,0 +1,15 @@
+import css from "./Title.module.css";
+
+interface TitleProps {
+  title: string;
+  description: string;
+}
+
+export default function Title({ title, description }: TitleProps) {
+  return (
+    <>
+      <h2 className={css.title}>{title}</h2>
+      <p className={css.description}>{description}</p>
+    </>
+  );
+}
